5bbb33cdb05fe3b1f9521ba06f4a9675fccbc5da
chenyifan <cccyyyf@foxmail.com>
2021-07-18 16:51:59 +0800

    贪心算法练习

--- leetcode/src/com/cyfhandsome/up/money/GreedyAlgorithm.java
+++ leetcode/src/com/cyfhandsome/up/money/GreedyAlgorithm.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 /**
  * @author cyf
  * @date 2021/7/12 13:44
+ * 贪心算法
  */
 public class GreedyAlgorithm {
 
@@ -169,6 +170,42 @@ public class GreedyAlgorithm {
         public static void main(String[] args) {
             int[][] array = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
             log.info(String.valueOf(eraseOverlapIntervals(array)));
+        }
+    }
+
+    /**
+     * 605种花问题
+     * 假设有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+     * <p>
+     * 给你一个整数数组flowerbed 表示花坛，由若干 0 和 1 组成，其中 0 表示没种植花，1 表示种植了花。另有一个数 n ，能否在不打破种植规则的情况下种入 n 朵花？能则返回 true ，不能则返回 false
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/can-place-flowers
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * 输入：flowerbed = [1,0,0,0,1], n = 1
+     * 输出：true
+     */
+    static class CanPlaceFlower {
+        static boolean canPlaceFlower(int[] flowerbed, int n) {
+            //从第一个开始遍历，如果当前位置有花，指向i+2的位置，依次类推
+            for (int i = 0; i < flowerbed.length && n > 0; ) {
+                //当初始位置有花时，向后+2位置
+                if (flowerbed[i] == 1) {
+                    i += 2;
+                } else if (flowerbed[i + 1] == 0 || i == flowerbed.length - 1) {
+                    n--;
+                    i += 2;
+                } else {
+                    i += 3;
+                }
+            }
+
+            return n <= 0;
+        }
+
+        public static void main(String[] args) {
+            int[] flowerbed = {1, 0, 0, 1, 0, 0,0,1};
+            log.info(String.valueOf(canPlaceFlower(flowerbed, 2)));
         }
     }
 }
